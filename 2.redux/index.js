caf95eebd6a6553d3b6a808b261733c89017fe78
Woo Dy <kjop7419@naver.com>
2022-03-26 20:07:39 +0900

    :memo:(인프런 제로초님 강의) thunk

--- 2.redux/index.js
+++ 2.redux/index.js
@@ -3,11 +3,11 @@ const reducer = require('./reducers');
 const { logIn, logOut } = require('./actions/user');
 const { addPost } = require('./actions/post');
 const initialState = {
-  user: {
-    isLoggingIn: false,
-    data: null,
-  },
-  post: [],
+  // user: {
+  //   isLoggingIn: false,
+  //   data: null,
+  // },
+  // post: [],
   comments: [],
   favorites: [],
   history: [],
@@ -15,11 +15,47 @@ const initialState = {
   followers: [],
 };
 
-const enhancer = compose(applyMiddleware());
+const firstMiddleware = (store) => (dispatch) => (action) => {
+  console.log('로깅', action);
+  dispatch(action);
+};
+
+const thunkMiddleware = (store) => (dispatch) => (action) => {
+  if (typeof action === 'function') {
+    // 비동기
+    return action(store.dispatch, store.getState);
+  }
+  return dispatch(action); // 동기
+};
+const enhancer = applyMiddleware(firstMiddleware, thunkMiddleware);
 
 const store = createStore(reducer, initialState, enhancer);
 
 // 밑에부터 리액트에서 컨트롤
+// store.dispatch(
+//   logIn({
+//     id: 1,
+//     name: 'woody',
+//     admin: true,
+//   })
+// );
+
+// console.log('logIn', store.getState());
+
+// store.dispatch(
+//   addPost({
+//     userId: 1,
+//     id: 1,
+//     content: '안녕하세요',
+//   })
+// );
+
+// console.log('post', store.getState());
+
+// store.dispatch(logOut());
+
+// console.log('logOut', store.getState());
+
 store.dispatch(
   logIn({
     id: 1,
@@ -28,18 +64,4 @@ store.dispatch(
   })
 );
 
-console.log('logIn', store.getState());
-
-store.dispatch(
-  addPost({
-    userId: 1,
-    id: 1,
-    content: '안녕하세요',
-  })
-);
-
-console.log('post', store.getState());
-
-store.dispatch(logOut());
-
-console.log('logOut', store.getState());
+console.log(store.getState());
